78311972db2cbec59070e87b626c65462050f379
Rahix <hws@denx.de>
2018-02-15 10:46:26 +0100

    Convert testcases
        - Make individual selftests available
          on the commandline
        - Start using new syntax (tb, *, params+)
        - Add tbot_clean_workdir
        - Make toolchain_env raise a custom exception
          if the toolchain does not exist

--- tbot/builtin/selftest.py
+++ tbot/builtin/selftest.py
@@ -4,6 +4,26 @@ TBOT self test
 """
 import tbot
 
+@tbot.testcase
+def selftest(tb: tbot.TBot) -> None:
+    """ TBot self test """
+    tb.log.log_msg("Testing shell functionality ...")
+    tb.call("selftest_noenv_shell")
+    tb.call("selftest_env_shell")
+    tb.call("selftest_board_shell")
+    tb.call("selftest_nested_boardshells")
+
+    tb.log.log_msg("Testing testcase functionality ...")
+    tb.call("selftest_testcase_calling")
+    tb.call("selftest_test_failures")
+    tb.call("selftest_wrong_parameter_type")
+
+    tb.log.log_msg("Testing logger ...")
+    tb.call("selftest_logger")
+
+    tb.log.log_msg("Testing builtin testcases ...")
+    tb.call("selftest_builtin_errors")
+
 def test_shell(shell: tbot.machine.Machine,
                has_printf: bool,
                has_echo_e: bool) -> None:
@@ -43,64 +63,61 @@ windows line ending\n", "%r does not match" % out
     return_code, _ = shell.exec("false")
     assert return_code == 1, f"Shell returned {return_code} instead of 1"
 
+
 @tbot.testcase
-def selftest(tb: tbot.TBot) -> None:
-    """ TBOT self test """
+def selftest_noenv_shell(tb: tbot.TBot) -> None:
+    """ Test noenv shell functionality """
+    with tb.machine(tbot.machine.MachineLabNoEnv()) as tbn:
+        test_shell(tbn.shell, True, True)
 
-    @tb.call
-    def noenv_shell(tb: tbot.TBot) -> None: #pylint: disable=unused-variable
-        """ Test noenv shell functionality """
-        with tb.machine(tbot.machine.MachineLabNoEnv()) as tbn:
-            test_shell(tbn.shell, True, True)
+        # Test if environment is actually not shared
+        tbn.shell.exec0("FOOBAR='avalue'")
+        out = tbn.shell.exec0("echo $FOOBAR")
+        assert out == "\n", f"Environment variable was set when it shouldn't: {repr(out)}"
 
-            # Test if environment is actually not shared
-            tbn.shell.exec0("FOOBAR='avalue'")
-            out = tbn.shell.exec0("echo $FOOBAR")
-            assert out == "\n", f"Environment variable was set when it shouldn't: {repr(out)}"
+@tbot.testcase
+def selftest_env_shell(tb: tbot.TBot) -> None:
+    """ Test env shell functionality """
+    with tb.machine(tbot.machine.MachineLabEnv()) as tbn:
+        test_shell(tbn.shell, True, True)
 
-    @tb.call
-    def env_shell(tb: tbot.TBot) -> None: #pylint: disable=unused-variable
-        """ Test env shell functionality """
-        with tb.machine(tbot.machine.MachineLabEnv()) as tbn:
-            test_shell(tbn.shell, True, True)
+        # Test if environment is actually working
+        tbn.shell.exec0("FOOBAR='avalue'")
+        out = tbn.shell.exec0("echo $FOOBAR")
+        assert out == "avalue\n", f"Environment variable was not set correctly: {repr(out)}"
 
-            # Test if environment is actually working
-            tbn.shell.exec0("FOOBAR='avalue'")
-            out = tbn.shell.exec0("echo $FOOBAR")
-            assert out == "avalue\n", f"Environment variable was not set correctly: {repr(out)}"
+@tbot.testcase
+def selftest_board_shell(tb: tbot.TBot) -> None:
+    """ Test board shell functionality """
+    with tb.with_boardshell() as tbn:
+        test_shell(tbn.boardshell,
+                   tbn.config["board.shell.support_printf", False],
+                   tbn.config["board.shell.support_echo_e", False])
 
-    @tb.call
-    def board_shell(tb: tbot.TBot) -> None: #pylint: disable=unused-variable
-        """ Test board shell functionality """
-        with tb.with_boardshell() as tbn:
-            test_shell(tbn.boardshell,
-                       tbn.config["board.shell.support_printf", False],
-                       tbn.config["board.shell.support_echo_e", False])
+@tbot.testcase
+def selftest_nested_boardshells(tb: tbot.TBot) -> None:
+    """ Test if tbot handles nested boardshells correctly """
+    with tb.with_boardshell() as tb1:
+        out = tb1.boardshell.exec0("echo Hello World")
+        assert out == "Hello World\n", "%r != 'Hello World'" % out
 
-    @tb.call
-    def nested_boardshells(tb: tbot.TBot) -> None: #pylint: disable=unused-variable
-        """ Test if tbot handles nested boardshells correctly """
-        with tb.with_boardshell() as tb1:
-            out = tb1.boardshell.exec0("echo Hello World")
-            assert out == "Hello World\n", "%r != 'Hello World'" % out
+        bs1 = tb1.boardshell
 
-            bs1 = tb1.boardshell
+        @tb1.call
+        def inner(tb: tbot.TBot) -> None: #pylint: disable=unused-variable
+            """ Second attempt of starting a boardshell """
+            with tb.with_boardshell() as tb2:
+                out = tb2.boardshell.exec0("echo Hello World")
+                assert out == "Hello World\n", "%r != 'Hello World'" % out
 
-            @tb1.call
-            def inner(tb: tbot.TBot) -> None: #pylint: disable=unused-variable
-                """ Second attempt of starting a boardshell """
-                with tb.with_boardshell() as tb2:
-                    out = tb2.boardshell.exec0("echo Hello World")
-                    assert out == "Hello World\n", "%r != 'Hello World'" % out
+                bs2 = tb2.boardshell
 
-                    bs2 = tb2.boardshell
+                assert bs1 is bs2, "%r is not %r" % (bs1, bs2)
 
-                    assert bs1 is bs2, "%r is not %r" % (bs1, bs2)
-
-    @tb.call
-    def logger(tb: tbot.TBot) -> None: #pylint: disable=unused-variable
-        """ Test logger """
-        text = """\
+@tbot.testcase
+def selftest_logger(tb: tbot.TBot) -> None:
+    """ Test logger """
+    text = """\
 Esse eligendi optio reiciendis. Praesentium possimus et autem.
 Ea in odit sit deleniti est nemo. Distinctio aspernatur facere aut culpa.
 
@@ -116,33 +133,71 @@ Facilis dolorem voluptate laudantium vero quis. Voluptatem quia ipsa
 quo pariatur iusto odio omnis. Nulla necessitatibus sapiente et
 perferendis dolor. Tempore at ipsum eos molestiae nobis error corporis."""
 
-        custom_ev = tbot.logger.CustomLogEvent(["custom", "event"], text,
-                                               tbot.logger.Verbosity.INFO,
-                                               {"text": text})
+    custom_ev = tbot.logger.CustomLogEvent(["custom", "event"], text,
+                                           tbot.logger.Verbosity.INFO,
+                                           {"text": text})
 
-        tb.log.log(custom_ev)
+    tb.log.log(custom_ev)
 
-    @tb.call
-    def testcase_calling(tb: tbot.TBot) -> None: #pylint: disable=unused-variable
-        """ Test calling testcases """
-        def a_testcase(_tb: tbot.TBot, param: str) -> int:
-            """ Sample testcase """
-            return int(param)
+@tbot.testcase
+def selftest_testcase_calling(tb: tbot.TBot) -> None:
+    """ Test calling testcases """
+    def a_testcase(_tb: tbot.TBot, param: str) -> int:
+        """ Sample testcase """
+        return int(param)
 
-        out = tb.call(a_testcase, param="123")
-        assert out == 123, "Testcase did not return 123: %r" % out
+    out = tb.call(a_testcase, param="123")
+    assert out == 123, "Testcase did not return 123: %r" % out
 
-    @tb.call
-    def test_failures(tb: tbot.TBot) -> None: #pylint: disable=unused-variable
-        """ Test a failing testcase """
-        def a_failing_testcase(_tb: tbot.TBot) -> None:
-            """ A testcase that fails """
-            raise Exception("failure?")
+@tbot.testcase
+def selftest_test_failures(tb: tbot.TBot) -> None:
+    """ Test a failing testcase """
+    def a_failing_testcase(_tb: tbot.TBot) -> None:
+        """ A testcase that fails """
+        raise Exception("failure?")
 
-        did_raise = False
-        try:
-            tb.call(a_failing_testcase)
-        except Exception as e: #pylint: disable=broad-except
-            assert e.args[0] == "failure?", "Testcase raised wrong exception"
-            did_raise = True
-        assert did_raise is True, "Testcase did not raise an exception"
+    did_raise = False
+    try:
+        tb.call(a_failing_testcase)
+    except Exception as e: #pylint: disable=broad-except
+        assert e.args[0] == "failure?", "Testcase raised wrong exception"
+        did_raise = True
+    assert did_raise is True, "Testcase did not raise an exception"
+
+@tbot.testcase
+def selftest_wrong_parameter_type(tb: tbot.TBot) -> None:
+    """ Test whether TBot detects wrong parameter types """
+
+    def testcase_with_int_param(_tb: tbot.TBot, *, param: int) -> int:
+        """ A testcase with an int parameter """
+        return param + 10
+
+    tb.log.log_debug("Testing with correct parameter type ...")
+    out = tb.call(testcase_with_int_param, param=20)
+    assert out == 30, "Testcase returned wrong result"
+
+    failed = False
+    try:
+        tb.log.log_debug("Testing with wrong parameter type ...")
+        out2 = tb.call(testcase_with_int_param, param="string_param")
+    except TypeError:
+        failed = True
+
+    assert failed is True, "TBot did not detect a wrong parameter type (result: %r)" % (out2,)
+
+@tbot.testcase
+def selftest_builtin_errors(tb: tbot.TBot) -> None:
+    """ Test whether builtin testcases properly error on invalid input """
+
+    def dummy(_tb: tbot.TBot) -> None:
+        """ A dummy testcase """
+        pass
+
+    tb.log.log_debug("Testing toolchain_env with nonexistant toolchain name ...")
+    try:
+        tb.call("toolchain_env", toolchain="a-toolchain-that-will-never-exist", and_then=dummy)
+    except Exception as e: #pylint: disable=broad-except
+        if e.__class__.__name__ != "UnknownToolchainException":
+            raise
+    else:
+        raise Exception("toolchain_env did not raise an UnknownToolchainException")
