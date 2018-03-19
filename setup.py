68801696a5404a104557317e5e465f6d0da9d3fb
Rahix <hws@denx.de>
2018-03-19 11:34:53 +0100

    Fix setup.py again

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name="tbot",
       version="0.1.0",
       packages=find_packages(),
-      requires=["paramiko", "argcomplete", "enforce"],
+      install_requires=["paramiko", "argcomplete", "enforce"],
       entry_points={
           "console_scripts": ["tbot = tbot.main:main"],
       },
