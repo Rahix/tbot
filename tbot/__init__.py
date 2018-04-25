6befcce6a39ca2caed0f1f9a139b2f44d93a4e67
Rahix <hws@denx.de>
2018-04-25 11:25:33 +0200

    Change call_then to return the function itself
    This allows reuse of the function at a later time

--- tbot/__init__.py
+++ tbot/__init__.py
@@ -86,7 +86,8 @@ class TBot:
         """
         def _decorator(f: typing.Callable) -> typing.Any:
             kwargs["and_then"] = f
-            return self.call(tc, **kwargs)
+            self.call(tc, **kwargs)
+            return f
         return _decorator
 
     def call(self, tc: typing.Union[str, typing.Callable], *,
