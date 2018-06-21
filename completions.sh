7689ef54b03b5c76cea45fdaa44a1b7ca202d3f4
Rahix <hws@denx.de>
2018-06-21 18:26:02 +0200

    Add completions for tbot-mgr
    Signed-off-by: Rahix <hws@denx.de>

--- completions.sh
+++ completions.sh
@@ -108,4 +108,125 @@ _tbot()
 } &&
 complete -F _tbot tbot
 
+_tbot-mgr()
+{
+    local cur prev words cword
+    _init_completion || return
+
+    # Completions for subcommands
+    local index=0
+    local subcommand=""
+    while [[ $index -lt ${#words[@]} ]]; do
+        local current_word="${words[$index]}"
+
+        if [[ "$current_word" == -* ]]; then
+            local index=$(($index + 1))
+            continue
+        fi
+
+        case "$current_word" in
+            "new")
+                local subcommand="new"
+                _filedir
+                return
+                ;;
+            "init")
+                local subcommand="init"
+                return
+                ;;
+            "add")
+                local subcommand="add"
+                if [[ "$prev" == "add" ]]; then
+                    COMPREPLY=( $( compgen -W 'board dummy-board
+                        lab dummy-lab dummies' -- "$cur" ) )
+                    return
+                else
+                    local index=$(($index + 1))
+                    local current_word="${words[$index]}"
+
+                    case "$current_word" in
+                        "board")
+                            local subcommand="add-board"
+                            [[ "$cur" == -* ]] && \
+                                COMPREPLY=( $( compgen -W '-h -s -f -n
+                                -l -d -t -0 -1 -c --help --no-interactive
+                                --script-mode --force --name --lab
+                                --default-lab --toolchain --on --off
+                                --connect' -- "$cur" ) ) && \
+                            return
+                            ;;
+                        "lab")
+                            local subcommand="add-lab"
+                            [[ "$cur" == -* ]] && \
+                                COMPREPLY=( $( compgen -W '-h -s -f -n
+                                -u -p -k -w --help --no-interactive
+                                --script-mode --force --name --user
+                                --password --keyfile --workdir' -- "$cur" ) ) && \
+                            return
+                            ;;
+                        "dummy-board")
+                            local subcommand="add-dummy-board"
+                            [[ "$cur" == -* ]] && \
+                                COMPREPLY=( $( compgen -W '-h -s -f -n
+                                -l --help --no-interactive
+                                --script-mode --force --name --lab' -- "$cur" ) ) && \
+                            return
+                            ;;
+                        "dummy-lab")
+                            local subcommand="add-dummy-lab"
+                            [[ "$cur" == -* ]] && \
+                                COMPREPLY=( $( compgen -W '-h -s -f -n
+                                --help --no-interactive --script-mode
+                                --force --name' -- "$cur" ) ) && \
+                            return
+                            ;;
+                        "dummies")
+                            local subcommand="add-dummy-both"
+                            [[ "$cur" == -* ]] && \
+                                COMPREPLY=( $( compgen -W '-h -s -f -n
+                                --help --no-interactive --script-mode
+                                --force --name' -- "$cur" ) ) && \
+                            return
+                            ;;
+                    esac
+                fi
+        esac
+
+        local index=$(($index + 1))
+    done
+
+    # Completions for flags
+    case "$prev" in
+        "-k"|"--keyfile")
+            _filedir
+            return
+            ;;
+        "-w"|"--workdir")
+            _filedir
+            return
+            ;;
+        "-l"|"--lab")
+            local labs=$(/bin/ls $labconfdir | grep \\.py | sed 's/\.py$//')
+            COMPREPLY=( $( compgen -W "$labs" -- "$cur") )
+            return
+            ;;
+        -*)
+            return
+            ;;
+    esac
+
+    # Other completions
+    if [[ $subcommand == "add-lab" ]]; then
+        _known_hosts
+        return
+    fi
+
+    if [[ "$cur" == -* ]]; then
+        COMPREPLY=( $( compgen -W '-h --help' -- "$cur" ) )
+    else
+        COMPREPLY=( $( compgen -W 'new init add' -- "$cur" ) )
+    fi
+} &&
+complete -F _tbot-mgr tbot-mgr
+
 #ex: filetype=sh
