4561a4625382d973d30b5d44db429ce3a6e619e1
Rahix <hws@denx.de>
2018-08-21 11:51:39 +0200

    Add interactive testcases

--- tbot/builtin/callable.py
+++ tbot/builtin/callable.py
@@ -1,1 +1,15 @@
-from selftest import *
+import tbot
+from selftest import *  # noqa
+
+
+@tbot.testcase
+def interactive_lab() -> None:
+    with tbot.acquire_lab() as lh:
+        lh.interactive()
+
+
+@tbot.testcase
+def interactive_build() -> None:
+    with tbot.acquire_lab() as lh:
+        with lh.default_build() as bh:
+            bh.interactive()
