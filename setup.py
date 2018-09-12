75d2de09cfc8b53ae2d6245b691f45cc48a31f1d
Rahix <rahix@rahix.de>
2018-09-12 17:13:32 +0200

    Add metadata file __about__.py
    Signed-off-by: Rahix <rahix@rahix.de>

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
+import typing
+import pathlib
 from setuptools import setup, find_packages
 import fastentrypoints  # noqa: F401
 
+tbot_dir = pathlib.Path(__file__).parent / "tbot"
+
+about: typing.Dict[str, str] = {}
+with open(tbot_dir / "__about__.py") as f:
+    exec(f.read(), about)
+
 setup(
-    name="tbot",
-    version="0.6.0-pre03",
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__summary__"],
+    author=about["__author__"],
+    author_email=about["__email__"],
     packages=find_packages(include=("tbot", "tbot.*")),
     install_requires=["paramiko", "termcolor2"],
     entry_points={
---
+++ tbot/__about__.py
@@ -0,0 +1,9 @@
+__all__ = ("__title__", "__summary__", "__version__", "__author__", "__email__")
+
+__title__ = "tbot"
+__summary__ = "Test and development automation tool, tailored for embedded"
+
+__version__ = "0.6.0-pre03"
+
+__author__ = "Rahix"
+__email__ = "hws@denx.de"
