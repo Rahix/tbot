ba57db1d6570c3642c5890907e14af836abb36fe
Rahix <hws@denx.de>
2018-12-20 11:35:20 +0100

    Switch to development version
    Signed-off-by: Harald Seiler <hws@denx.de>

--- tbot/__about__.py
+++ tbot/__about__.py
@@ -3,7 +3,7 @@ __all__ = ("__title__", "__summary__", "__version__", "__author__", "__email__")
 __title__ = "tbot"
 __summary__ = "Test and development automation tool, tailored for embedded"
 
-__version__ = "0.6.5"
+__version__ = "0.6.5-dev"
 
 __author__ = "Rahix"
 __email__ = "hws@denx.de"
