4fb8133f73ad22d4b553d85dd8dc37acc65e3811
Rahix <hws@denx.de>
2019-07-19 13:44:21 +0200

    machine-v2: Reintroduce some log-events
    Signed-off-by: Harald Seiler <hws@denx.de>

--- tbot/machine_v2/board/board.py
+++ tbot/machine_v2/board/board.py
@@ -1,6 +1,7 @@
 import abc
 import contextlib
 import typing
+import tbot
 from .. import machine
 
 
@@ -16,7 +17,17 @@ class PowerControl(machine.Initializer):
     @contextlib.contextmanager
     def _init_machine(self) -> typing.Iterator:
         try:
+            tbot.log.EventIO(
+                ["board", "on", self.name],
+                tbot.log.c("POWERON").bold + f" ({self.name})",
+                verbosity=tbot.log.Verbosity.QUIET,
+            )
             self.poweron()
             yield None
         finally:
+            tbot.log.EventIO(
+                ["board", "off", self.name],
+                tbot.log.c("POWEROFF").bold + f" ({self.name})",
+                verbosity=tbot.log.Verbosity.QUIET,
+            )
             self.poweroff()
--- tbot/machine_v2/connector/common.py
+++ tbot/machine_v2/connector/common.py
@@ -12,11 +12,9 @@ class SubprocessConnector(connector.Connector):
     __slots__ = ()
 
     def _connect(self) -> channel.Channel:
-        tbot.log.message("--> connecting")
         return channel.SubprocessChannel()
 
     def clone(self: SelfSubprocess) -> SelfSubprocess:
-        tbot.log.message("--> subp clone")
         return type(self)()
 
 
@@ -31,11 +29,10 @@ class SerialConsoleConnector(connector.Connector):
 
     @contextlib.contextmanager
     def _connect(self) -> typing.Iterator[channel.Channel]:
-        tbot.log.message("--> serial connecting start")
         with self.mach.clone() as cloned:
             ch = cloned.ch.take()
-            tbot.log.message("--> serial connecting")
-            ch.sendline("picocom -b 115200 /dev/ttyUSB0")
+            tbot.log_event.command(self.mach.name, "picocom -b 115200 /dev/ttyUSB0")
+            ch.sendline("picocom -b 115200 /dev/ttyUSB0", read_back=True)
             yield ch
 
     def clone(self: SelfSerial) -> SelfSerial:
