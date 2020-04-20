Rahix/tbot

commit 5e1c5878e4c67cddd354e14066210b4e33f37c87

path: Fix __fspath__ returning wrong info
The __fspath__() method defined by the os.PathLike interface is
assumed to return a representation of the path on the local file-
system.  As tbot-paths are potentially non-local the existence of
this method does not make sense in the first place anyway.
    However, as tbot's Path type is derived from pathlib.Path, it
inherits this method which then returns the str(path) representation.
    This commit ensures that any attempt to use __fspath__() will
result in an exception, to make certain that it is not possible to
introduce silent bugs by functions from the python standard library
accepting a tbot-path.

Signed-off-by: Harald Seiler <hws@denx.de>

tbot/machine/linux/path.py
@@ -147,3 +147,6 @@ class Path(pathlib.PurePosixPath, typing.Generic[H]):
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self._host!r}, {super().__str__()!r})"
+
+    def __fspath__(self) -> str:
+        raise NotImplementedError("__fspath__ does not exist for tbot paths!")

commit b7a2a138a3533bcb4be765ab9c881fa8cbfd8345

linux.path: Fixup __fspath__
It is bad practice [1] to use NotImplementedError() to indicate that
a method is not supported by a subclass.  Instead, one should override
the method with None explicitly.

[1]: https://docs.python.org/3/library/exceptions.html#NotImplementedError

Fixes: 5e1c5878e4c6 ("path: Fix __fspath__ returning wrong info")
Signed-off-by: Harald Seiler <hws@denx.de>

tbot/machine/linux/path.py
@@ -312,5 +312,6 @@ class Path(pathlib.PurePosixPath, typing.Generic[H]):
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self._host!r}, {super().__str__()!r})"
 
-    def __fspath__(self) -> str:
-        raise NotImplementedError("__fspath__ does not exist for tbot paths!")
+    # __fspath__ does not make sense for tbot paths as they don't represent
+    # a path on the local filesystem.
+    __fspath__ = None  # type: ignore