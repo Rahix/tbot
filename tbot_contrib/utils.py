89972842a67002da1050476b9d5c93ce758067fb
Rahix <hws@denx.de>
2020-08-03 12:18:14 +0200

    contrib: utils: Add testcase to find IP address of machine
    In times where most hosts have many IP addresses, this is not as trivial
    as one would like.  This testcase approaches the problem by trying to
    find the IP-address, the host would use on a certain route.
    
    By default, this is the route to reach a theoretical host at 1.0.0.0.
    This will yield a sensible result in *most* cases but of course will not
    always be the address you want.  For more fine-grained control you can
    pass a `route_target`.  This is the IP-address of this theoretical host
    to reach.
    
    Signed-off-by: Harald Seiler <hws@denx.de>

--- tbot_contrib/utils.py
+++ tbot_contrib/utils.py
@@ -13,9 +13,11 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import re
+import typing
 
 from tbot.machine import linux
-import typing
+from tbot.tc import shell
 
 
 _SERVICES_CACHE: typing.Dict[str, typing.Dict[str, bool]] = {}
@@ -40,3 +42,53 @@ def ensure_sd_unit(lnx: linux.LinuxShell, services: typing.List[str]) -> None:
             lnx.exec0("sudo", "systemctl", "start", s)
 
         _SERVICES_CACHE[lnx.name][s] = True
+
+
+_IP_CACHE: typing.Dict[typing.Tuple[linux.LinuxShell, str], str] = {}
+
+
+def find_ip_address(
+    lnx: linux.LinuxShell,
+    route_target: typing.Optional[str] = None,
+    force: bool = False,
+) -> str:
+    """
+    Find out an IP-address of a host.
+
+    In times where most hosts have many IP addresses, this is not as trivial as
+    one would like.  This testcase approaches the problem by trying to find the
+    IP-address, the host would use on a certain route.
+
+    By default, this is the route to reach a theoretical host at ``1.0.0.0``.
+    This will yield a sensible result in *most* cases but of course will not
+    always be the address you want.  For more fine-grained control you can pass
+    a ``route_target``.  This is the IP-address of this theoretical host to reach.
+
+    :param linux.LinuxShell lnx: The host to work on.
+    :param str route_target: An optional route target.  Defaults to ``1.0.0.0``.
+    :param bool force: By default, this testcase caches results for faster
+        lookup when called multiple times.  This parameter enforces a re-check
+        which might be useful when the network configuration on ``lnx``
+        changed.
+    :rtype: str
+    :returns: The IP-address which was found.
+    """
+    if route_target is None:
+        # 1 equals to 1.0.0.0 which will probably yield a sensible route in
+        # most cases.
+        route_target = "1"
+
+    if (lnx, route_target) not in _IP_CACHE:
+        if shell.check_for_tool(lnx, "ip"):
+            output = lnx.exec0("ip", "route", "get", route_target, linux.Pipe, "cat")
+            match = re.match(
+                r"\S+ (?:via \S+ )?dev \S+ src (\S+).*", output, re.DOTALL,
+            )
+            assert match is not None, f"Failed to parse `ip route` output ({output!r})!"
+            ip = match.group(1)
+        else:
+            raise NotImplementedError("Found no way to find out ip-address")
+
+        _IP_CACHE[(lnx, route_target)] = ip
+
+    return _IP_CACHE[(lnx, route_target)]
