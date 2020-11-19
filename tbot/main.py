6770159d316e2523ea856d4e3899ed81157c393f
Rahix <hws@denx.de>
2020-11-19 14:32:43 +0100

    main: Enter tbot.ctx context-manager
    Enter the tbot.ctx context's context-manager while executing testcases.
    This will run the context's cleanup code after testcase execution
    completes.  Currently, said cleanup code just warns about dangling
    instances in the context.
    
    Signed-off-by: Harald Seiler <hws@denx.de>

--- tbot/main.py
+++ tbot/main.py
@@ -317,31 +317,32 @@ def main() -> None:  # noqa: C901
         )
 
     try:
-        for tc in args.testcase:
-            func = testcases[tc]
+        with tbot.ctx:
+            for tc in args.testcase:
+                func = testcases[tc]
 
-            if len(args.testcase) == 1:
-                params = parameters
-            else:
-                # Filter parameter list if multiple testcases are scheduled to run
+                if len(args.testcase) == 1:
+                    params = parameters
+                else:
+                    # Filter parameter list if multiple testcases are scheduled to run
 
-                try:
-                    func_code = getattr(func, "__wrapped__").__code__
-                except AttributeError:
-                    func_code = func.__code__
-                # Get list of argument names
-                argspec = inspect.getargs(func_code)
+                    try:
+                        func_code = getattr(func, "__wrapped__").__code__
+                    except AttributeError:
+                        func_code = func.__code__
+                    # Get list of argument names
+                    argspec = inspect.getargs(func_code)
 
-                params = {}
-                for name, value in parameters.items():
-                    if argspec.varkw is not None or name in argspec.args:
-                        params[name] = value
-                    else:
-                        tbot.log.warning(
-                            f"Parameter {name!r} not defined for testcase {tc!r}, ignoring ..."
-                        )
+                    params = {}
+                    for name, value in parameters.items():
+                        if argspec.varkw is not None or name in argspec.args:
+                            params[name] = value
+                        else:
+                            tbot.log.warning(
+                                f"Parameter {name!r} not defined for testcase {tc!r}, ignoring ..."
+                            )
 
-            func(**params)
+                func(**params)
     except Exception as e:  # noqa: E722
         import traceback
 
