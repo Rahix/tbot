Rahix/tbot

commit 567a75a59434430ca8ba23cdd025961c76e13738

treewide: Implement from_context() for most connectors
Provide a sensible default implementation of from_context() for most
connectors shipped with tbot.

Signed-off-by: Harald Seiler <hws@denx.de>

tbot/machine/connector/ssh.py
@@ -23,6 +23,8 @@ from . import connector
 from .. import linux, channel
 from ..linux import auth
 
+Self = typing.TypeVar("Self", bound="SSHConnector")
+
 
 class SSHConnector(connector.Connector):
     """
@@ -147,6 +149,16 @@ class SSHConnector(connector.Connector):
             self.host = host
         else:
             self.host = tbot.acquire_local()  # type: ignore
+
+    @classmethod
+    @contextlib.contextmanager
+    def from_context(
+        cls: typing.Type[Self], ctx: "tbot.Context"
+    ) -> typing.Iterator[Self]:
+        with contextlib.ExitStack() as cx:
+            lh = cx.enter_context(ctx.request(tbot.role.LabHost))
+            m = cx.enter_context(cls(lh))  # type: ignore
+            yield m
 
     @contextlib.contextmanager
     def _connect(self) -> typing.Iterator[channel.Channel]:

tbot/machine/board/board.py
@@ -99,6 +99,9 @@ class Board(shell.RawShell):
     pass
 
 
+M = typing.TypeVar("M", bound=machine.Machine)
+
+
 class Connector(connector.Connector):
     def __init__(self, board: typing.Union[Board, channel.Channel]) -> None:
         if not (isinstance(board, Board) or isinstance(board, channel.Channel)):
@@ -107,6 +110,14 @@ class Connector(connector.Connector):
             )
         self._board = board
         self.host = getattr(board, "host", None)
+
+    @classmethod
+    @contextlib.contextmanager
+    def from_context(cls: typing.Type[M], ctx: "tbot.Context") -> typing.Iterator[M]:
+        with contextlib.ExitStack() as cx:
+            b = cx.enter_context(ctx.request(tbot.role.Board))
+            m = cx.enter_context(cls(b))  # type: ignore
+            yield m
 
     @contextlib.contextmanager
     def _connect(self) -> typing.Iterator[channel.Channel]:

tbot/machine/board/linux.py
@@ -185,6 +185,16 @@ class LinuxUbootConnector(connector.Connector, LinuxBootLogin):
     def __init__(self, b: typing.Union[board.Board, board.UBootShell]) -> None:
         self._b = b
 
+    @classmethod
+    @contextlib.contextmanager
+    def from_context(
+        cls: typing.Type[Self], ctx: "tbot.Context"
+    ) -> typing.Iterator[Self]:
+        with contextlib.ExitStack() as cx:
+            ub = cx.enter_context(ctx.request(tbot.role.BoardUBoot))
+            m = cx.enter_context(cls(ub))  # type: ignore
+            yield m
+
     @contextlib.contextmanager
     def _connect(self) -> typing.Iterator[channel.Channel]:
         with contextlib.ExitStack() as cx:

tbot/machine/connector/common.py
@@ -18,6 +18,7 @@ import abc
 import contextlib
 import typing
 
+import tbot
 import tbot.error
 from tbot import machine  # noqa: F401
 from .. import channel, linux
@@ -50,6 +51,12 @@ class SubprocessConnector(connector.Connector):
     """
 
     __slots__ = ()
+
+    @classmethod
+    @contextlib.contextmanager
+    def from_context(cls: typing.Type[M], ctx: "tbot.Context") -> typing.Iterator[M]:
+        with cls() as m:
+            yield m
 
     def _connect(self) -> channel.Channel:
         return channel.SubprocessChannel()
@@ -112,6 +119,14 @@ class ConsoleConnector(connector.Connector):
             respecitive board to the constructor here.
         """
         self.host = mach
+
+    @classmethod
+    @contextlib.contextmanager
+    def from_context(cls: typing.Type[M], ctx: "tbot.Context") -> typing.Iterator[M]:
+        with contextlib.ExitStack() as cx:
+            lh = cx.enter_context(ctx.request(tbot.role.LabHost))
+            m = cx.enter_context(cls(lh))  # type: ignore
+            yield m
 
     @contextlib.contextmanager
     def _connect(self) -> typing.Iterator[channel.Channel]:

tbot/machine/connector/paramiko.py
@@ -19,6 +19,7 @@ import getpass
 import paramiko
 import pathlib
 import typing
+import contextlib
 
 import tbot
 from .. import channel
@@ -141,6 +142,14 @@ class ParamikoConnector(connector.Connector):
         if other is not None:
             self._client = other._client
             self._config = other._config
+
+    @classmethod
+    @contextlib.contextmanager
+    def from_context(
+        cls: typing.Type[Self], ctx: "tbot.Context"
+    ) -> typing.Iterator[Self]:
+        with cls() as m:
+            yield m
 
     def _connect(self) -> channel.Channel:
         if self._client is None:

tbot_contrib/connector/pyserial.py
@@ -1,6 +1,7 @@
 import abc
 import os
 import typing
+import contextlib
 
 import tbot.error
 from tbot.machine import channel, connector, linux
@@ -80,6 +81,9 @@ class PyserialChannel(channel.Channel):
         super().__init__(PyserialChannelIO(port, baudrate))
 
 
+M = typing.TypeVar("M", bound="PyserialConnector")
+
+
 class PyserialConnector(connector.Connector):
     """
     Connect to a console connected to **localhost** (i.e. the host tbot is
@@ -114,6 +118,12 @@ class PyserialConnector(connector.Connector):
                 "PyserialConnector can only use a localhost host (got {host!r})!"
             )
         self.host = host
+
+    @classmethod
+    @contextlib.contextmanager
+    def from_context(cls: typing.Type[M], ctx: "tbot.Context") -> typing.Iterator[M]:
+        with cls() as m:
+            yield m
 
     def _connect(self) -> channel.Channel:
         return PyserialChannel(self.serial_port, self.baudrate)

commit f9f2fe8a2512f16d6ddf06b3f72daef85e238741

connector: ssh: Fix contexts breaking ssh lab-host usecase
The SSHConnector supports two modes of operation.  It can either connect
from the localhost to a remote (if no argument is passed to the
constructor) or if can connect from the lab-host to a remove (if
a lab-host instance is passed to the constructor).

The from_context() implementation added in commit 567a75a59434
("treewide: Implement from_context() for most connectors") only really
implements the latter mode.  Trying to invoke the former (e.g. by making
the lab-host use an SSHConnector) will lead to an infinite recursion as
the from_context() implementation will recursively try to request an
instance of its own class (the lab-host).

As a workaround, enable this one specific usecase again by checking if
the class the SSHConnector is initializing for is registered as the
lab-host and if so, not re-requesting itself.

In the future, it might be a better option to introduce a way to
configure which role the SSHConnector tries to use as the base for its
connection.

Fixes: 567a75a59434 ("treewide: Implement from_context() for most connectors")
Signed-off-by: Harald Seiler <hws@denx.de>

tbot/machine/connector/ssh.py
@@ -154,7 +154,9 @@ class SSHConnector(connector.Connector):
         cls: typing.Type[Self], ctx: "tbot.Context"
     ) -> typing.Iterator[Self]:
         with contextlib.ExitStack() as cx:
-            lh = cx.enter_context(ctx.request(tbot.role.LabHost))
+            lh = None
+            if isinstance(cls, ctx.get_machine_class(tbot.role.LabHost)):
+                lh = cx.enter_context(ctx.request(tbot.role.LabHost))
             m = cx.enter_context(cls(lh))  # type: ignore
             yield m
 