1f5a8d3049a5ba9ce1b415389783960d1c129a29
Rahix <hws@denx.de>
2021-09-10 13:03:29 +0200

    selftest: contrib: Add more utils tests
    Add some more smoketests to ensure the helpers in tbot_contrib.utils
    actually work as intended.
    
    Signed-off-by: Harald Seiler <hws@denx.de>

--- selftest/tests/contrib/test_utils.py
+++ selftest/tests/contrib/test_utils.py
@@ -3,7 +3,7 @@ import typing
 from typing import Callable, ContextManager, Iterator
 
 import pytest
-from conftest import AnyLinuxShell
+import testmachines
 
 import tbot
 import tbot_contrib.utils
@@ -13,10 +13,10 @@ if typing.TYPE_CHECKING:
 
 
 @pytest.fixture
-def testdir_builder(any_linux_shell: AnyLinuxShell) -> "TestDir":
+def testdir_builder(tbot_context: tbot.Context) -> "TestDir":
     @contextlib.contextmanager
     def inner() -> Iterator[tbot.machine.linux.Path]:
-        with any_linux_shell() as linux_shell:
+        with tbot_context.request(testmachines.Localhost) as linux_shell:
             testdir = linux_shell.workdir / "utils-tests"
             linux_shell.exec0("rm", "-rf", testdir)
             linux_shell.exec0("mkdir", testdir)
@@ -42,3 +42,18 @@ def test_hashcmp(testdir_builder: "TestDir") -> None:
         assert not tbot_contrib.utils.hashcmp(a, c)
         assert not tbot_contrib.utils.hashcmp(b, c)
         assert not tbot_contrib.utils.hashcmp(a, missing)
+
+
+def test_strip_ansi_escapes_smoke() -> None:
+    in_str = "\x1B[33mSome colored text!\x1B[0m"
+    expected_str = "Some colored text!"
+
+    out_str = tbot_contrib.utils.strip_ansi_escapes(in_str)
+
+    assert out_str == expected_str
+
+
+def test_find_ip_address(tbot_context: tbot.Context) -> None:
+    with tbot_context.request(testmachines.Localhost) as lo:
+        # Check whether it is able to find a "default" address
+        tbot_contrib.utils.find_ip_address(lo)
