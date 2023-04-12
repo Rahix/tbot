Rahix/tbot

commit f9f2fe8a2512f16d6ddf06b3f72daef85e238741

connector: ssh: Fix contexts breaking ssh lab-host usecase
The SSHConnector supports two modes of operation.  It can either connect
from the localhost to a remote (if no argument is passed to the
constructor) or if can connect from the lab-host to a remove (if
a lab-host instance is passed to the constructor).

The from_context() implementation added in commit 567a75a59434
("treewide: Implement from_context() for most connectors") only really
implements the latter mode.  Trying to invoke the former (e.g. by making
the lab-host use an SSHConnector) will lead to an infinite recursion as
the from_context() implementation will recursively try to request an
instance of its own class (the lab-host).

As a workaround, enable this one specific usecase again by checking if
the class the SSHConnector is initializing for is registered as the
lab-host and if so, not re-requesting itself.

In the future, it might be a better option to introduce a way to
configure which role the SSHConnector tries to use as the base for its
connection.

Fixes: 567a75a59434 ("treewide: Implement from_context() for most connectors")
Signed-off-by: Harald Seiler <hws@denx.de>

tbot/machine/connector/ssh.py
@@ -154,7 +154,9 @@ class SSHConnector(connector.Connector):
         cls: typing.Type[Self], ctx: "tbot.Context"
     ) -> typing.Iterator[Self]:
         with contextlib.ExitStack() as cx:
-            lh = cx.enter_context(ctx.request(tbot.role.LabHost))
+            lh = None
+            if isinstance(cls, ctx.get_machine_class(tbot.role.LabHost)):
+                lh = cx.enter_context(ctx.request(tbot.role.LabHost))
             m = cx.enter_context(cls(lh))  # type: ignore
             yield m
 

commit 0587e9d600f1ea859d51346cbc8c1f179a554ce3

connector: ssh: Fix connecting via lab-host for real
The previous attempt at fixing the special-casing of lab-host vs.
local-host based connections in commit f9f2fe8a2512 ("connector: ssh:
Fix contexts breaking ssh lab-host usecase") actually never worked as
intended.

Fix it by using the proper subclass check.

Fixes: f9f2fe8a2512 ("connector: ssh: Fix contexts breaking ssh lab-host usecase")
Signed-off-by: Harald Seiler <hws@denx.de>

tbot/machine/connector/ssh.py
@@ -157,7 +157,7 @@ class SSHConnector(connector.Connector):
     ) -> typing.Iterator[Self]:
         with contextlib.ExitStack() as cx:
             lh = None
-            if isinstance(cls, ctx.get_machine_class(tbot.role.LabHost)):
+            if not issubclass(cls, ctx.get_machine_class(tbot.role.LabHost)):
                 lh = cx.enter_context(ctx.request(tbot.role.LabHost))
             m = cx.enter_context(cls(lh))  # type: ignore
             yield typing.cast(Self, m)