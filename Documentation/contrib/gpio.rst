9074d90b263fa0c90a38a5c738a8c3b30caf1fd8
Alex <130048160+bauer-alex-174@users.noreply.github.com>
2023-06-21 12:15:54 +0200

    contrib: gpio: Add docs and pulse method

---
+++ Documentation/contrib/gpio.rst
@@ -0,0 +1,8 @@
+.. py:module:: tbot_contrib.gpio
+
+``tbot_contrib.gpio``
+=======================
+A module containing gpio integration
+
+.. autoclass:: tbot_contrib.gpio.Gpio
+    :members: set_direction, get_direction, set_active_low, get_active_low, set_value, get_value, toggle_value
--- tbot_contrib/gpio.py
+++ tbot_contrib/gpio.py
@@ -1,8 +1,17 @@
+import time
 from tbot.machine import linux
+from typing import Optional
 
 
 class Gpio:
     def __init__(self, host: linux.LinuxShell, gpio_number: int):
+        """Initialize GPIO
+
+        Raspberry Pi Model 2 and above use as gpio_number their BCM GPIO Numbers
+
+        :param linux.LinuxShell host: Linux Shell
+        :param int gpio_number: GPIO Number
+        """
 
         self.host = host
         self.gpio_number = gpio_number
@@ -12,10 +21,17 @@ class Gpio:
         self._direction = self.get_direction()
 
     def _export(self) -> None:
+        """Exports GPIO"""
         if not self._gpio_path.is_dir():
             (self._gpio_sysclass_path / "export").write_text(str(self.gpio_number))
 
     def set_direction(self, direction: str) -> None:
+        """Set Direction
+
+        Sets direction to ``in`` or ``out``
+
+        :param str direction: Direction to set
+        """
         assert direction in ["in", "out"], f"Unsupported GPIO direction: {direction!r}"
         if self._direction == direction:
             return
@@ -24,20 +40,70 @@ class Gpio:
         self._direction = direction
 
     def set_active_low(self, value: bool) -> None:
+        """Set Active Low
+
+        When Active Low is ``True``, the GPIO triggers on a ``LOW`` singnal, else on a ``HIGH`` signal
+
+        :param bool value: Value
+        """
         (self._gpio_path / "active_low").write_text("1" if value else "0")
 
     def get_active_low(self) -> bool:
+        """Get Active Low
+
+        Reads value and returns direction ``in`` or ``out``
+
+        :rtype: bool
+        :returns: Value
+        """
         return (self._gpio_path / "active_low").read_text().strip() != "0"
 
     def get_direction(self) -> str:
+        """Get Direction of GPIO
+
+        Reads direction and returns direction ``in`` or ``out``
+
+        :rtype: str
+        :returns: Value
+        """
         return (self._gpio_path / "direction").read_text().strip()
 
     def set_value(self, value: bool) -> None:
+        """Set Value
+
+        Sets value to ``HIGH`` and ``LOW``
+
+        :param bool value: Value to set
+        """
         if self._direction == "in":
             raise Exception("Can't set a GPIO which is not an output")
         (self._gpio_path / "value").write_text("1" if value else "0")
 
     def get_value(self) -> bool:
+        """Get Value
+
+        Reads value and returns value ``True`` or ``False``
+
+        :rtype: bool
+        :returns: Value
+        """
         if not (self._direction == "in"):
             raise Exception("Can't get a value from a GPIO which is not an input")
         return (self._gpio_path / "value").read_text().strip() != "0"
+
+    def pulse(self, on_time: float, off_time: Optional[float] = None) -> None:
+        """Toggle GPIO
+
+        Toggles GPIO Pin between ``HIGH`` and ``LOW``
+
+        :param float on_time: Time GPIO is ``HIGH``
+        :param float off_time: Time GPIO is ``LOW``. Defaults to ``on_time``
+        """
+
+        if off_time is None:
+            off_time = on_time
+
+        self.set_value(True)
+        time.sleep(on_time)
+        self.set_value(False)
+        time.sleep(off_time)
